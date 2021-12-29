12e419df60723c0702c0efc9141333f0f42a9c2f
Chadi Hamrouni <chadi.hamrouni@medtech.tn>
2021-12-29 22:57:03 +0100

    get top 10 stories

---
+++ hacker-news/server/routes/stories.routes.js
@@ -0,0 +1,39 @@
+let MongoClient = require('mongodb').MongoClient;
+
+// Specify your local MongoDB instance
+var url = "mongodb://localhost:27017/";
+
+// Get all stories
+app.get('/api/stories', (req, res) => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("put database name here");
+        dbo.collection("collection name here").findOne({
+            id: req.params.id,
+            by: req.params.by,
+            title: req.params.title,
+            score: req.body.score
+        }, 
+        function(err, result) {
+            if (err) throw err;
+            res.json(result);
+            db.close();
+        });
+    });
+});
+
+
+// This section will help you get a list of 10 stories.
+recordRoutes.route("/api/stories").get(async function (req, res) {
+  const dbConnect = dbo.getDb();
+  dbConnect
+    .collection("collection name")
+    .find({}).limit(10)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching stories!");
+     } else {
+        res.json(result);
+      }
+    });
+})
